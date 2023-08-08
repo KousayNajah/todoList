34f7f0cf106d62af3975e36633d8064d08669c38
koussay <koussaynajah8@gmail.com>
2023-08-08 10:35:09 +0100

    feat: add input todo files and components

---
+++ src/components/inputTodo.js
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { Button } from "reactstrap";
+
+
+const InputTodo = () => {
+  const navigate = useNavigate();
+
+  const [description, setDescription] = useState("");
+
+  const onSubmitForm = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        "http://localhost:5000/api/v1/addTodo",
+        {
+          description: description,
+        }
+      );
+      navigate("/ListeTodos");
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+  
+ 
+  return (
+    <React.Fragment>
+      <form
+        onSubmit={onSubmitForm}
+        style={{ alignItems: "center", textAlign: "center" }}
+      >
+        <div className="d-flex flex-column justify-content-center">
+          <h1 className="text-center" style={{ textAlign: "center" }}>
+            Pern Todo Tist
+          </h1>
+          <div style={{ position: "relative", left: "25%", width: "50%" }}>
+            <input
+              type="text"
+              className="form-control"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              placeholder="enter your todo"
+            />
+          </div>
+          <div>
+            <Button
+              color="success"
+              style={{
+                width: "70px",
+                margin: 10,
+                height: "40px",
+                border: "transparent",
+              }}
+              onClick={onSubmitForm}
+            >
+              Add
+            </Button>
+          </div>
+        </div>
+      </form>
+    </React.Fragment>
+  );
+};
+export default InputTodo;
